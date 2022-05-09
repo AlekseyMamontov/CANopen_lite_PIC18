018fbfa2ae4d7dc590c4111802ecc1ee68e3c06d
Aleksey <35818127+AlekseyMamontov@users.noreply.github.com>
2022-05-09 14:39:38 +0300

    Add files via upload

---
+++ CanOpen_rele220v_12/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MPLABXProjects.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPLABXProjects.production.hex
