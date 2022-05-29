b5a0bb32fa1dfb39a1a411b0e4e5e97b20e17376
Aleksey <35818127+AlekseyMamontov@users.noreply.github.com>
2022-05-29 21:54:27 +0300

    Add files via upload

--- CanOpen_rele220v_12/CANOPEN_lite.h
+++ CanOpen_rele220v_12/CANOPEN_lite.h
@@ -1,8 +1,8 @@
 /* 
  * File:   CANopenLite.h
  * Author: aleksey
- * rethinking )
- * Created on 27 05 2022 14:38
+ *
+ * Created on 25 ??? 2022 ?., 14:38
  */
 
 #ifndef CANOPENLITE_H
@@ -51,9 +51,9 @@ extern "C" {
 #define INTENGER8   02
 #define INTENGER16  03
 #define INTENGER32  04	
-#define UNSIGNED8	05
-#define UNSIGNED16	06
-#define UNSIGNED32	07
+#define UNSIGNED8	 05
+#define UNSIGNED16	 06
+#define UNSIGNED32	 07
 
 #define OD_NULL 		0
 #define OD_DOMAIN 		2
@@ -63,33 +63,21 @@ extern "C" {
 #define OD_VAR 			7	
 #define OD_ARRAY 		8	
 
-#define ERROR_sub_index	0x06090011
-#define ERROR_no_object	0x06020000
+#define ERROR_sub_index 0x06090011
+#define ERROR_no_object 0x06020000
 #define ERROR_no_open   0x06010000
 #define ERROR_ALL_OD_table 0x08000000
 
 struct OD_table{
 	
-	
+/* index */	
 uint16_t	index;
-uint8_t		sub_index;
-/*attr
-RW - Read and write access
-WO-  Write only
-RO - Read only
-CONST- Read only, Data is constant
- */
-uint8_t		attr;
-/*
-NULL 			0
-OD_DOMAIN 		2
-OD_RECORD 		9
-OD_DEFSTRUCT	6
-OD_DEFTYPE 		5
-OD_VAR 			7	
-OD_ARRAY 		8
- */
-uint8_t		object;
+/*sub_index*/
+uint8_t	sub_index;
+
+/* NULL 0/  OD_DOMAIN 2 / OD_RECORD 9 /OD_DEFSTRUCT 6
+OD_DEFTYPE 5 / OD_VAR 7 / OD_ARRAY 8  */
+uint8_t	object;
 /*
 BOOLEAN		01
 INTENGER8   02
@@ -99,7 +87,7 @@ UNSIGNED8	05
 UNSIGNED16	06
 UNSIGNED32	07
  */
-uint8_t		type;
+uint8_t	type;
 /*
 BOOLEAN	   *;
 INTENGER8  *;
@@ -110,11 +98,13 @@ UNSIGNED16 *;
 UNSIGNED32 *;
 void (*func)(*,*);
  */
-void		*data;
+void*	data;
+
+/* lsizeOf()*/
+uint8_t	n_bayt;
 
 //struct OD_table *next;
 //struct OD_table *prev;
-
 };
 
 
@@ -124,9 +114,11 @@ struct CAN_Node{
 /*search */	
 struct OD_table *first_od_table;
 struct OD_table *current_od_table;
-uint32_t 		error_search;	
-uint16_t 		index; 
-uint8_t 		sub_index;
+uint32_t	error_search;	
+uint16_t	index; 
+uint8_t 	sub_index;
+
+
 /**/	
 	
 
@@ -135,64 +127,115 @@ uint8_t 		sub_index;
 
 
 /* Search OD_table and return struct OD_table *
- * complete simple list iteration
- * */
-struct OD_table 
-*OD_search (struct CAN_Node *node, uint16_t index, uint8_t sub_index;){
-	
-struct OD_table	*tab = node->first_od_table;		
-uint32_t *status_error = node->error_search;
-	 *status_error = ERROR_no_object;
+ * complete simple list iteration */
 
-    while  (tab->index != 0){
-		if (tab->index == index && tab->sub_index == sub_index){
-			if(tab->data == NULL){*status_error = ERROR_ALL_OD_table;
-					}else{*status_error = 0;};break;}	
-		if (tab->index > index){*status_error = ERROR_sub_index; break;}	    
-		tab++;}
+struct OD_table *OD_serch_index0(struct OD_table *tab,uint16_t index){
 	
-node->current_od_table = tab;	
+  uint8_t err = 1;	
+  
+	while  (tab->index <= index){ 
+		if (tab->index == 0)break;   
+		if (tab->index == index){
+			if(tab->sub_index == 0)err=0;//its ok 	
+		break;}
+	tab++;};
+	if (err) tab = NULL;
+	
+ return tab;};
+ 
+ /*version 1*/
+ 
+struct OD_table *OD_search_sub_index(struct OD_table *tab,uint8_t sub_index){
+	
+	if (tab->object == OD_VAR ||  tab->object == OD_DEFTYPE ) return tab;
+	if (tab->data != NULL && *((uint8_t*)tab->data) < sub_index ){
+		
+		tab += sub_index;
+		
+	 }else{tab = NULL;};	 
+	 
 return tab;}
+
+/* version  2*/
+
+struct OD_table *OD_search_subindex(struct OD_table *tab,uint8_t sub_index){
+	
+  uint16_t index = tab->index;	
+  uint8_t err = 1;
+  
+	while(tab->index == index){
+	       if  (tab->sub_index == sub_index){err=0; break;}
+	       tab++;};
+	       
+	if (err) tab = NULL;  	
+		
+return tab;};
 
 /* Search OD_table and return struct OD_table *
  * complete simple list iteration
+ * Metod 1
  * */
 
-struct OD_table *OD_search_N (struct CAN_Node *node, uint16_t index, uint8_t sub_index;){
+uint8_t OD_search_N( struct CAN_Node *node){
 
-struct OD_table	*tab = node->first_od_table;	
-uint32_t		node->error_search = 0;
-uint8_t			sub,erorr = 0; 
+struct OD_table *tab = node->current_od_table;
+uint16_t index = node->index;
+uint8_t   sub_index = node->sub_index;
+uint8_t   error =2;
 
-   while(tab->index){ 
-	   
-		if (tab->index == index && tab->sub_index == 0){
-				
-			 if(tab->object = OD_ARRAY || 
-				tab->object== OD_DEFSTRUCT){
-                 		
-                 		if(*((uint8_t*)(tab->data)) < sub_index){
-								node->error_search = ERORR_sub_index;
-								break;}
-							
-								tab += sub_index;
-								
-						if (tab->sub_index != sub_index){ 						
-								node->error_search = ERRR_ALL_OD_table;
-								break;}
-								};
-							
-		if(tab->data == NULL) node->error_search = ERORR_no_object;
-		break;
-		
-		}else{tab += (tab->sub_index)+1;}
-};
-		
+    while  (tab->index <= index){ 
+	
+	if (tab->index == 0)break;   
+	if (tab->index == index){
+	      if (tab->sub_index != sub_index){error = 1;}else{error=0; break;}; //its ok
+	};
+	
+	tab++;
+    };
+    switch(error){
+	case 0:node->error_search= 0;break;
+	case 1:node->error_search= ERROR_sub_index;break;
+	default : node->error_search= ERROR_no_object;tab =NULL;break;
+   };		
 node->current_od_table = tab;		
-return tab;
+return error;
+}
+
+/* Search OD_table  */
+
+struct OD_table *
+OD_search_all( struct CAN_Node *node, uint16_t index,uint8_t sub_index){
+	node->index = index;
+	node->sub_index = sub_index;
+	node->current_od_table = node->first_od_table;
+	OD_search_N (node); 	
+	return node->current_od_table;
 };
 
 
+/* Search OD_table and return struct OD_table *
+ * complete simple list iteration
+ * Metod 2
+ * */
+
+uint8_t OD_search__num (struct OD_table **ntab,uint16_t index,uint8_t sub_index ){
+
+struct OD_table *tab = *ntab;
+
+uint8_t   error =2;
+
+    while  (tab->index <= index){ 	
+	if (tab->index == 0)break;   
+	if (tab->index == index){
+	      if (tab->sub_index != sub_index){error = 1;
+	      }else{error=0; break;}; //its ok
+	};	
+	tab++;
+    };
+    
+    *ntab = tab ;		
+	
+return error;}
 
 
 
